436513fbce72b4d2f1f4e93f39a1d521a89788b1
Xola13 <xolamagatya86@gmail.com>
2022-05-12 12:48:02 +0200

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6" >
+    <h1>About Todo Plans</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+       Doloremque doloribus quis, quas a sed nulla natus nihil totam pariatur corrupti cum velit minus non quia voluptatum in laborum incidunt voluptatibus.</p>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+         Delectus magni, dignissimos qui aspernatur eos in sequi quam praesentium quaerat cumque, libero facere,
+          rem excepturi a consectetur fugiat esse hic eveniet!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+         Delectus magni, dignissimos qui aspernatur eos in sequi quam praesentium quaerat cumque, libero facere,
+          rem excepturi a consectetur fugiat esse hic eveniet!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+         Delectus magni, dignissimos qui aspernatur eos in sequi quam praesentium quaerat cumque, libero facere,
+          rem excepturi a consectetur fugiat esse hic eveniet!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+         Delectus magni, dignissimos qui aspernatur eos in sequi quam praesentium quaerat cumque, libero facere,
+          rem excepturi a consectetur fugiat esse hic eveniet!</p>
   </div>
 </template>
